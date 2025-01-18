376769a63a6b886730b9d97cace9679b95882172
alfiyan100 <44922351+alfiyan100@users.noreply.github.com>
2025-01-18 21:34:03 +0700

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,454 @@
+questions = [
+    {
+        "question": "Siapa Admin Redzone yang paling ganteng?",
+        "answers": ["Brian"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang biasa ditemukan di meja kerja",
+        "answers": ["Pena", "Kertas", "Laptop", "Cangkir", "Telepon"]
+    },
+    {
+        "question": "Sebutkan Nama Admin Terganteng",
+        "answers": ["Brian"]  
+    },
+    {
+        "question": "Sebutkan makanan yang biasanya disantap saat sarapan",
+        "answers": ["Roti", "Sereal", "Nasi Goreng", "Telur", "Pancake"]
+    },
+    {
+        "question": "Sebutkan buah yang sering dijadikan jus",
+        "answers": ["Jeruk", "Mangga", "Apel", "Jambu", "Alpukat"]
+    },
+    {
+        "question": "Nama lain dari kemaluan pria adalah.",
+        "answers": ["Kontol", "Tytyd"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang biasa ditemukan di meja kerja",
+        "answers": ["Pena", "Kertas", "Laptop", "Cangkir", "Telepon"]
+    },
+    {
+        "question": "Sebutkan makanan yang biasanya disantap saat sarapan",
+        "answers": ["Roti", "Sereal", "Nasi Goreng", "Telur", "Pancake"]
+    },
+    {
+        "question": "Sebutkan hewan yang biasa dipelihara di rumah",
+        "answers": ["Kucing", "Anjing", "Ikan", "Burung", "Hamster"]
+    },
+    {
+        "question": "Sebutkan buah yang sering dijadikan jus",
+        "answers": ["Jeruk", "Mangga", "Apel", "Jambu", "Alpukat"]
+    },
+    {
+        "question": "Sebutkan warna pelangi",
+        "answers": ["Merah", "Jingga", "Kuning", "Hijau", "Biru", "Nila", "Ungu"]
+    },
+    {
+        "question": "Sebutkan hari dalam seminggu",
+        "answers": ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"]
+    },
+    {
+        "question": "Sebutkan nama bulan dalam setahun",
+        "answers": ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+    },
+    {
+        "question": "Sebutkan olahraga yang menggunakan bola",
+        "answers": ["Sepak Bola", "Basket", "Bola Voli", "Tenis", "Pingpong"]
+    },
+    {
+        "question": "Sebutkan benua yang ada di dunia",
+        "answers": ["Asia", "Afrika", "Amerika Utara", "Amerika Selatan", "Antartika", "Eropa", "Australia"]
+    },
+    {
+        "question": "Sebutkan ibu kota negara di Asia",
+        "answers": ["Tokyo", "Beijing", "New Delhi", "Seoul", "Bangkok"]
+    },
+    {
+        "question": "Sebutkan binatang laut",
+        "answers": ["Ikan", "Lumba-Lumba", "Hiu", "Kepiting", "Ubub-Ubur"]
+    },
+    {
+        "question": "Sebutkan planet di tata surya",
+        "answers": ["Merkurius", "Venus", "Bumi", "Mars", "Jupiter", "Saturnus", "Uranus", "Neptunus"]
+    },
+    {
+        "question": "Sebutkan negara di Eropa",
+        "answers": ["Inggris", "Perancis", "Jerman", "Italia", "Spanyol"]
+    },
+    {
+        "question": "Sebutkan alat musik tiup",
+        "answers": ["Suling", "Saxophone", "Klarinet", "Trompet", "Tuba"]
+    },
+    {
+        "question": "Sebutkan ibu kota negara di Afrika",
+        "answers": ["Kairo", "Nairobi", "Addis Ababa", "Abuja", "Pretoria"]
+    },
+    {
+        "question": "Sebutkan jenis pohon buah-buahan",
+        "answers": ["Mangga", "Apel", "Pisang", "Jeruk", "Kelapa"]
+    },
+    {
+        "question": "Sebutkan jenis ikan yang bisa dimakan",
+        "answers": ["Nila", "Lele", "Salmon", "Tuna", "Sarden"]
+    },
+    {
+        "question": "Sebutkan jenis burung yang bisa terbang",
+        "answers": ["Elang", "Merpati", "Sparrow", "Kakaktua", "Rajawali"]
+    },
+    {
+        "question": "Sebutkan nama-nama superhero",
+        "answers": ["Spiderman", "Batman", "Superman", "Wonder Woman", "Iron Man"]
+    },
+    {
+        "question": "Sebutkan jenis sayuran hijau",
+        "answers": ["Bayam", "Kangkung", "Sawi", "Selada", "Brokoli"]
+    },
+    {
+        "question": "Sebutkan nama alat transportasi",
+        "answers": ["Mobil", "Motor", "Sepeda", "Kereta", "Pesawat"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang terbuat dari kayu",
+        "answers": ["Meja", "Kursi", "Lemari", "Pintu", "Jendela"]
+    },
+    {
+        "question": "Sebutkan jenis kue yang sering disajikan di pesta",
+        "answers": ["Kue Tart", "Cupcake", "Brownies", "Cheesecake", "Donat"]
+    },
+    {
+        "question": "Sebutkan alat dapur yang sering digunakan",
+        "answers": ["Kompor", "Panci", "Pisau", "Sendok", "Blender"]
+    },
+    {
+        "question": "Sebutkan benda yang ada di dalam kamar tidur",
+        "answers": ["Kasur", "Bantal", "Selimut", "Lemari", "Lampu"]
+    },
+    {
+        "question": "Sebutkan hewan yang hidup di kutub",
+        "answers": ["Penguin", "Beruang Kutub", "Anjing Laut", "Walrus", "Rubah Arktik"]
+    },
+    {
+        "question": "Sebutkan jenis film",
+        "answers": ["Drama", "Komedi", "Aksi", "Horor", "Animasi"]
+    },
+    {
+        "question": "Sebutkan jenis bunga yang harum",
+        "answers": ["Melati", "Mawar", "Anggrek", "Kamboja", "Kenanga"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang biasanya ada di meja makan",
+        "answers": ["Piring", "Gelas", "Sendok", "Mangkuk", "Tisu"]
+    },
+    {
+        "question": "Sebutkan jenis olahraga air",
+        "answers": ["Renang", "Selancar", "Ski Air", "Polo Air", "Menyelam"]
+    },
+    {
+        "question": "Sebutkan jenis kopi",
+        "answers": ["Americano", "Latte", "Espresso", "Cappuccino", "Macchiato"]
+    },
+    {
+        "question": "Sebutkan negara di benua Amerika",
+        "answers": ["Amerika Serikat", "Kanada", "Meksiko", "Brasil", "Argentina"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang digunakan saat musim dingin",
+        "answers": ["Jaket", "Sarung Tangan", "Topi", "Syal", "Sepatu Bot"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang biasanya dibawa saat liburan",
+        "answers": ["Pakaian", "Kamera", "Paspor", "Uang", "Sunblock"]
+    },
+    {
+        "question": "Sebutkan jenis makanan cepat saji",
+        "answers": ["Burger", "Pizza", "Fried Chicken", "Hotdog", "Sandwich"]
+    },
+    {
+        "question": "Sebutkan jenis pakaian musim panas",
+        "answers": ["Kaos", "Celana Pendek", "Baju Renang", "Kacamata Hitam", "Topi Pantai"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang bisa digunakan untuk menulis",
+        "answers": ["Pena", "Pensil", "Spidol", "Kapur", "Krayon"]
+    },
+    {
+        "question": "Sebutkan jenis minuman dingin",
+        "answers": ["Es Teh", "Es Jeruk", "Smoothie", "Milkshake", "Lemonade"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang biasa ditemukan di meja kerja",
+        "answers": ["Pena", "Kertas", "Laptop", "Cangkir", "Telepon"]
+    },
+    {
+        "question": "Sebutkan makanan yang biasanya disantap saat sarapan",
+        "answers": ["Roti", "Sereal", "Nasi Goreng", "Telur", "Pancake"]
+    },
+    {
+        "question": "Sebutkan hewan yang biasa dipelihara di rumah",
+        "answers": ["Kucing", "Anjing", "Ikan", "Burung", "Hamster"]
+    },
+    {
+        "question": "Sebutkan buah yang sering dijadikan jus",
+        "answers": ["Jeruk", "Mangga", "Apel", "Jambu", "Alpukat"]
+    },
+    {
+        "question": "Sebutkan warna pelangi",
+        "answers": ["Merah", "Jingga", "Kuning", "Hijau", "Biru", "Nila", "Ungu"]
+    },
+    {
+        "question": "Sebutkan hari dalam seminggu",
+        "answers": ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"]
+    },
+    {
+        "question": "Sebutkan nama bulan dalam setahun",
+        "answers": ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+    },
+    {
+        "question": "Sebutkan olahraga yang menggunakan bola",
+        "answers": ["Sepak Bola", "Basket", "Bola Voli", "Tenis", "Pingpong"]
+    },
+    {
+        "question": "Sebutkan benua yang ada di dunia",
+        "answers": ["Asia", "Afrika", "Amerika Utara", "Amerika Selatan", "Antartika", "Eropa", "Australia"]
+    },
+    {
+        "question": "Sebutkan ibu kota negara di Asia",
+        "answers": ["Tokyo", "Beijing", "New Delhi", "Seoul", "Bangkok"]
+    },
+    {
+        "question": "Sebutkan binatang laut",
+        "answers": ["Ikan", "Lumba-Lumba", "Hiu", "Kepiting", "Ubub-Ubur"]
+    },
+    {
+        "question": "Sebutkan planet di tata surya",
+        "answers": ["Merkurius", "Venus", "Bumi", "Mars", "Jupiter", "Saturnus", "Uranus", "Neptunus"]
+    },
+    {
+        "question": "Sebutkan negara di Eropa",
+        "answers": ["Inggris", "Perancis", "Jerman", "Italia", "Spanyol"]
+    },
+    {
+        "question": "Sebutkan alat musik tiup",
+        "answers": ["Suling", "Saxophone", "Klarinet", "Trompet", "Tuba"]
+    },
+    {
+        "question": "Sebutkan ibu kota negara di Afrika",
+        "answers": ["Kairo", "Nairobi", "Addis Ababa", "Abuja", "Pretoria"]
+    },
+    {
+        "question": "Sebutkan jenis pohon buah-buahan",
+        "answers": ["Mangga", "Apel", "Pisang", "Jeruk", "Kelapa"]
+    },
+    {
+        "question": "Sebutkan jenis ikan yang bisa dimakan",
+        "answers": ["Nila", "Lele", "Salmon", "Tuna", "Sarden"]
+    },
+    {
+        "question": "Sebutkan jenis burung yang bisa terbang",
+        "answers": ["Elang", "Merpati", "Sparrow", "Kakaktua", "Rajawali"]
+    },
+    {
+        "question": "Sebutkan nama-nama superhero",
+        "answers": ["Spiderman", "Batman", "Superman", "Wonder Woman", "Iron Man"]
+    },
+    {
+        "question": "Sebutkan jenis sayuran hijau",
+        "answers": ["Bayam", "Kangkung", "Sawi", "Selada", "Brokoli"]
+    },
+    {
+        "question": "Sebutkan nama alat transportasi",
+        "answers": ["Mobil", "Motor", "Sepeda", "Kereta", "Pesawat"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang terbuat dari kayu",
+        "answers": ["Meja", "Kursi", "Lemari", "Pintu", "Jendela"]
+    },
+    {
+        "question": "Sebutkan jenis kue yang sering disajikan di pesta",
+        "answers": ["Kue Tart", "Cupcake", "Brownies", "Cheesecake", "Donat"]
+    },
+    {
+        "question": "Sebutkan alat dapur yang sering digunakan",
+        "answers": ["Kompor", "Panci", "Pisau", "Sendok", "Blender"]
+    },
+    {
+        "question": "Sebutkan benda yang ada di dalam kamar tidur",
+        "answers": ["Kasur", "Bantal", "Selimut", "Lemari", "Lampu"]
+    },
+    {
+        "question": "Sebutkan hewan yang hidup di kutub",
+        "answers": ["Penguin", "Beruang Kutub", "Anjing Laut", "Walrus", "Rubah Arktik"]
+    },
+    {
+        "question": "Sebutkan jenis film",
+        "answers": ["Drama", "Komedi", "Aksi", "Horor", "Animasi"]
+    },
+    {
+        "question": "Sebutkan jenis bunga yang harum",
+        "answers": ["Melati", "Mawar", "Anggrek", "Kamboja", "Kenanga"]
+    },
+    {
+        "question": "Sebutkan sesuatu yang biasanya ada di meja makan",
+        "answers": ["Piring", "Gelas", "Sendok", "Mangkuk", "Tisu"]
+    },
+    {    
+        "question": "Sebutkan member BTS",
+        "answers": ["RM", "Jin", "Suga", "J-Hope", "Jimin", "V", "Jungkook"]
+    },
+    {
+        "question": "Sebutkan lagu terkenal dari BLACKPINK",
+        "answers": ["Ddu-Du Ddu-Du", "Kill This Love", "How You Like That", "As If It's Your Last", "Whistle"]
+    },
+    {
+        "question": "Sebutkan nama grup K-pop yang terkenal",
+        "answers": ["BTS", "BLACKPINK", "EXO", "TWICE", "Red Velvet"]
+    },
+    {
+        "question": "Sebutkan agensi K-pop yang terkenal",
+        "answers": ["SM Entertainment", "YG Entertainment", "JYP Entertainment", "Big Hit Entertainment", "Cube Entertainment"]
+    },
+    {
+        "question": "Sebutkan member EXO",
+        "answers": ["Suho", "Xiumin", "Lay", "Baekhyun", "Chen", "Chanyeol", "D.O.", "Kai", "Sehun"]
+    },
+    {
+        "question": "Sebutkan lagu terkenal dari TWICE",
+        "answers": ["TT", "Cheer Up", "Fancy", "Likey", "What is Love?"]
+    },
+    {
+        "question": "Sebutkan member Red Velvet",
+        "answers": ["Irene", "Seulgi", "Wendy", "Joy", "Yeri"]
+    },
+    {
+        "question": "Sebutkan girl group K-pop yang terkenal",
+        "answers": ["BLACKPINK", "TWICE", "Red Velvet", "Girls' Generation", "ITZY"]
+    },
+    {
+        "question": "Sebutkan boy group K-pop yang terkenal",
+        "answers": ["BTS", "EXO", "NCT", "SEVENTEEN", "STRAY KIDS"]
+    },
+    {
+        "question": "Sebutkan karakter utama dalam anime Naruto",
+        "answers": ["Naruto", "Sasuke", "Sakura", "Kakashi", "Itachi"]
+    },
+    {
+        "question": "Sebutkan anggota Straw Hat Pirates dalam anime One Piece",
+        "answers": ["Luffy", "Zoro", "Nami", "Usopp", "Sanji", "Chopper", "Robin", "Franky", "Brook", "Jinbei"]
+    },
+    {
+        "question": "Sebutkan ras utama dalam anime Attack on Titan",
+        "answers": ["Titan", "Manusia", "Eldian", "Marleyan"]
+    },
+    {
+        "question": "Sebutkan karakter dalam anime My Hero Academia",
+        "answers": ["Deku", "Bakugo", "Todoroki", "All Might", "Uravity"]
+    },
+    {
+        "question": "Sebutkan Pokemon starter generasi pertama",
+        "answers": ["Bulbasaur", "Charmander", "Squirtle"]
+    },
+    {
+        "question": "Sebutkan karakter dalam anime Dragon Ball Z",
+        "answers": ["Goku", "Vegeta", "Gohan", "Piccolo", "Frieza"]
+    },
+    {
+        "question": "Sebutkan anime dengan tema isekai",
+        "answers": ["Sword Art Online", "Re:Zero", "Overlord", "Konosuba", "No Game No Life"]
+    },
+    {
+        "question": "Sebutkan karakter dalam anime Death Note",
+        "answers": ["Light", "L", "Misa", "Ryuk", "Near"]
+    },
+    {
+        "question": "Sebutkan Shinigami dalam anime Bleach",
+        "answers": ["Ichigo", "Rukia", "Renji", "Byakuya", "Toshiro"]
+    },
+    {
+        "question": "Sebutkan serangan utama dari anime Naruto",
+        "answers": ["Rasengan", "Chidori", "Kage Bunshin", "Amaterasu", "Susanoo"]
+    },
+    {
+        "question": "Sebutkan karakter dalam anime Fairy Tail",
+        "answers": ["Natsu", "Lucy", "Gray", "Erza", "Happy"]
+    },
+    {
+        "question": "Sebutkan penyihir dalam anime Black Clover",
+        "answers": ["Asta", "Yuno", "Noelle", "Yami", "Licht"]
+    },
+    {
+        "question": "Sebutkan pahlawan dalam anime One Punch Man",
+        "answers": ["Saitama", "Genos", "Bang", "Tornado", "King"]
+    },
+    {
+        "question": "Sebutkan karakter dalam anime Tokyo Ghoul",
+        "answers": ["Kaneki", "Touka", "Ayato", "Amon", "Rize"]
+    },
+    {
+        "question": "Sebutkan karakter dalam anime Fullmetal Alchemist",
+        "answers": ["Edward", "Alphonse", "Winry", "Roy", "Riza"]
+    },
+    {
+        "question": "Sebutkan jenis Zanpakuto dalam anime Bleach",
+        "answers": ["Zangetsu", "Sode no Shirayuki", "Hyorinmaru", "Senbonzakura", "Zabimaru"]
+    },
+    {
+        "question": "Sebutkan bijuu dalam anime Naruto",
+        "answers": ["Kurama", "Shukaku", "Matatabi", "Isobu", "Son Goku"]
+    },
+    {
+        "question": "Sebutkan karakter dalam anime Attack on Titan",
+        "answers": ["Eren", "Mikasa", "Armin", "Levi", "Erwin"]
+    },
+    {
+        "question": "Sebutkan karakter dalam anime Hunter x Hunter",
+        "answers": ["Gon", "Killua", "Leorio", "Kurapika", "Hisoka"]
+    },
+    {
+        "question": "Sebutkan ras di anime One Piece",
+        "answers": ["Manusia", "Raksasa", "Mermaid", "Fishmen", "Mink"]
+    },
+    {
+        "question": "Sebutkan musim dalam setahun:",
+        "answers": ["Musim Semi", "Musim Panas", "Musim Gugur", "Musim Dingin"]
+    },
+    {
+        "question": "Sebutkan negara di Asia:",
+        "answers": ["Jepang", "Cina", "India", "Korea Selatan"]
+    },
+    {
+        "question": "Sebutkan genre film:",
+        "answers": ["Aksi", "Komedi", "Drama", "Horor", "Romantis"]
+    },
+    {
+        "question": "Sebutkan alat musik:",
+        "answers": ["Gitar", "Piano", "Drum", "Biola", "Flute"]
+    },
+    {
+        "question": "Sebutkan bahasa yang populer di dunia:",
+        "answers": ["Inggris", "Mandarin", "Spanyol", "Prancis"]
+    },
+    {
+        "question": "Sebutkan sumber energi terbarukan:",
+        "answers": ["Matahari", "Angin", "Air", "Biomassa"]
+    },
+    {
+        "question": "Sebutkan jenis kendaraan:",
+        "answers": ["Mobil", "Sepeda Motor", "Sepeda", "Bus", "Kereta"]
+    },
+    {
+        "question": "Sebutkan pekerjaan di kantor:",
+        "answers": ["Manajer", "Sekretaris", "Akuntan", "Resepsionis", "IT Support"]
+    },
+    {
+        "question": "Sebutkan hobi yang populer:",
+        "answers": ["Membaca", "Memasak", "Berkebun", "Menonton Film", "Bermain Musik"]
+    },
+    {
+        "question": "Sebutkan genre musik:",
+        "answers": ["Pop", "Rock", "Jazz", "Klasik"]
+    },
+    {
+        "question": "Sebutkan aplikasi media sosial:",
+        "answers": ["Facebook", "Instagram", "Twitter", "TikTok", "Snapchat"]
+    }
+]
